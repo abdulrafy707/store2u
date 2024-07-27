2e0eeb0dd13ff151d168cc3078d896655bafd65b
Abdul Rafy <mrafybasra2020@gmail.com>
2024-07-27 17:56:03 +0500

    first commit

--- src/app/admin/pages/page.js
+++ src/app/admin/pages/page.js
@@ -1,27 +1,27 @@
-// pages/admin/index.js
-'use client';
-import { useState } from 'react';
-import Layout from '../../components/Layout';
-import BranchesPage from '../branches/page';
+// // pages/admin/index.js
+// 'use client';
+// import { useState } from 'react';
+// import Layout from '../../components/Layout';
+// import BranchesPage from '../branches/page';
 
-const AdminPage = () => {
-  const [activeComponent, setActiveComponent] = useState('branches');
+// const AdminPage = () => {
+//   const [activeComponent, setActiveComponent] = useState('branches');
 
-  const renderActiveComponent = () => {
-    switch (activeComponent) {
-      case 'branches':
-        return <BranchesPage />;
-      // Add other cases for different components
-      default:
-        return <div>Welcome to the Admin Panel</div>;
-    }
-  };
+//   const renderActiveComponent = () => {
+//     switch (activeComponent) {
+//       case 'branches':
+//         return <BranchesPage />;
+//       // Add other cases for different components
+//       default:
+//         return <div>Welcome to the Admin Panel</div>;
+//     }
+//   };
 
-  return (
-    <Layout setActiveComponent={setActiveComponent}>
-      {renderActiveComponent()}
-    </Layout>
-  );
-};
+//   return (
+//     <Layout setActiveComponent={setActiveComponent}>
+//       {renderActiveComponent()}
+//     </Layout>
+//   );
+// };
 
-export default AdminPage;
+// export default AdminPage;
